f27a8f798b9905cbcb96e6b127ad4b98f9d276e6
nrennie <nrennie35@gmail.com>
2023-01-26 17:30:16 +0000

    use points instead of sf

--- R/population_density/uk.R
+++ R/population_density/uk.R
@@ -1,36 +1,49 @@
-library(R.utils)
-library(tidyverse)
-library(sf)
-library(showtext)
+#library(R.utils)
+#library(tidyverse)
+#library(sf)
+#library(showtext)
+library(ggplot2)
 library(rayshader)
 library(scico)
 
+options(rgl.useNULL=TRUE)
+
 # load fonts
-font_add(family = "Hill House",
-         regular = "G:\\My Drive\\GitHub\\misc\\R\\population_density\\Hill_House.otf")
+font_add_google("Poiret One", "poiret")
 showtext_auto()
 
 # download data from https://data.humdata.org/dataset/kontur-population-united-kingdom-of-great-britain-and-northern-ireland
-gunzip("R/population_density/kontur_population_GB_20220630.gpkg.gz", remove=FALSE)
-kontur <- st_read("R/population_density/kontur_population_GB_20220630.gpkg")
+# gunzip("R/population_density/kontur_population_GB_20220630.gpkg.gz", remove=FALSE)
+kontur <- sf::st_read("R/population_density/kontur_population_GB_20220630.gpkg")
+
+# convert to points
+plot_data = kontur |> 
+  sf::st_centroid() |> 
+  sf::st_coordinates() |> 
+  tibble::as_tibble() |> 
+  dplyr::mutate(population = kontur$population)
 
 # make ggplot map
 g <- ggplot() +
-  geom_sf(data = kontur, 
-          aes(fill = population,
-              colour = population)) +
-  scale_fill_scico(direction = 1, trans = "pseudo_log", palette = "lajolla") +
-  scale_color_scico(direction = 1, trans = "pseudo_log", palette = "lajolla") +
+  geom_point(data = plot_data,
+             aes(x = X, y = Y, colour = population),
+             size = 0.3) +
+  scale_color_scico(direction = 1,
+                    trans = "pseudo_log",
+                    palette = "lajolla",
+                    limits = c(0, 18000)) +
   labs(title = "United Kingdom") +
-  theme_void() +
   theme(legend.position = "none",
-        plot.title = element_text(colour = "#0B0B45", family = "Hill House", size = 34, hjust = 0.5),
-        plot.margin = margin(10, 10, 10, 10), 
+        plot.title = element_text(colour = "#0B0B45", size = 16, hjust = 0.5),
+        plot.margin = margin(10, 20, 10, 20), 
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        axis.text = element_blank(),
+        panel.grid = element_blank(),
         plot.background = element_rect(fill = "white", colour = "white"),
         panel.background = element_rect(fill = "white", colour = "white")) 
+g
 
 # make 3d
-rayshader::plot_gg(g,
-                   height_aes = "fill",
-                   preview = TRUE)
+rayshader::plot_gg(g, height_aes = "colour")
 rayshader::render_snapshot("uk.png", clear = TRUE)
